a87a45837685e7074ee83b7d7b42aee22be9f4a7
Trần Hoàng Hải ( Bin Dev ) <haihoang1312004@gmail.com>
2026-05-11 15:13:33 +0700

    price number

---
+++ ThePriceNumberChallenge/src/Main.java
@@ -0,0 +1,32 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int primeCount = 0;
+
+        for (int number = 2; number <= 1000; number++) {
+            if (isPrime(number)) {
+                System.out.println(number);
+                primeCount++;
+
+                if (primeCount == 3) {
+                    break;
+                }
+            }
+        }
+    }
+
+    public static boolean isPrime(int wholeNumber) {
+        if (wholeNumber <= 2) {
+            return (wholeNumber == 2);
+        }
+
+        for (int divisor = 2; divisor <= wholeNumber / 2; divisor++) {
+            if (wholeNumber % divisor == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
