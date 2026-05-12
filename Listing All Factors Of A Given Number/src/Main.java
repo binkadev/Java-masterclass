0ea1b8e43c74aacb69d8a16d03f1f286ec744813
Trần Hoàng Hải ( Bin Dev ) <haihoang1312004@gmail.com>
2026-05-12 21:58:23 +0700

    Update Main.java

--- Listing All Factors Of A Given Number/src/Main.java
+++ Listing All Factors Of A Given Number/src/Main.java
@@ -1,15 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+// Listing All Factors Of A Given Number
+//Write a method named printFactors with one parameter of type int named number.
+//
+//If number is < 1, the method should print "Invalid Value".
+//
+//The method should print all factors of the number. A factor of a number is an integer which divides that number wholly (i.e. without leaving a remainder).
+//
+//For example, 3 is a factor of 6 because 3 fully divides 6 without leaving a remainder. In other words 6 / 3 = 2.
+//
+//
+//
+//EXAMPLE INPUT/OUTPUT:
+//
+//printFactors(6); → should print 1 2 3 6
+//
+//printFactors(32); → should print 1 2 4 8 16 32
+//
+//printFactors(10); → should print 1 2 5 10
+//
+//printFactors(-1); → should print "Invalid Value" since number is < 1
+//
+//
+//
+//HINT: Use a while or for loop.
+//
+//
+//
+//NOTE: When printing numbers, each number can be in its own line. They don't have to be separated by a space.
+//
+//For example, the printout for printFactors(10); can be:
+//
+//1
+//2
+//5
+//10
+//NOTE: The method printFactors​ should be defined as public static like we have been doing so far in the course.
+//
+//NOTE: Do not add a main method to the solution code.
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
-}
