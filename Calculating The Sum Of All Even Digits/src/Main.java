c9fa7d179d6f5bfc7782f7d07ef452e3d93eaa12
Trần Hoàng Hải ( Bin Dev ) <haihoang1312004@gmail.com>
2026-05-12 21:15:51 +0700

    Update Main.java

--- Calculating The Sum Of All Even Digits/src/Main.java
+++ Calculating The Sum Of All Even Digits/src/Main.java
@@ -1,15 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+// Calculating The Sum Of All Even Digits In An Integer
+//Write a method named getEvenDigitSum with one parameter of type int called number.
+//
+//The method should return the sum of the even digits within the number.
+//
+//If the number is negative, the method should return -1 to indicate an invalid value.
+//
+//
+//
+//EXAMPLE INPUT/OUTPUT:
+//
+//getEvenDigitSum(123456789); → should return 20 since 2 + 4 + 6 + 8 = 20
+//
+//getEvenDigitSum(252); → should return 4 since 2 + 2 = 4
+//
+//getEvenDigitSum(-22); → should return -1 since the number is negative
+//
+//
+//
+//NOTE: The method getEvenDigitSum ​should be defined as public static like we have been doing so far in the course.
+//
+//NOTE: Do not add a main method to the solution code.
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class EvenDigitSum {
+    public static int getEvenDigitSum(int number) {
+        if (number < 0) {
+            return -1;
         }
+
+        int sum = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            if (digit % 2 == 0) {
+                sum += digit;
+
+            }
+
+            number = number / 10;
+        }
+        return sum;
     }
 }
