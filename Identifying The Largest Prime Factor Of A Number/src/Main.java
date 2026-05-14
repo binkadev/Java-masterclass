861a1c16ba7ab7e7c6d8adf521ac0d58fc48e145
Trần Hoàng Hải ( Bin Dev ) <haihoang1312004@gmail.com>
2026-05-14 15:11:44 +0700

    Update Main.java

--- Identifying The Largest Prime Factor Of A Number/src/Main.java
+++ Identifying The Largest Prime Factor Of A Number/src/Main.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+// Identifying The Largest Prime Factor Of A Number
+//Write a method named getLargestPrime with one parameter of type int named number.
+//
+//If the number is negative or does not have any prime numbers, the method should return -1 to indicate an invalid value.
+//
+//
+//
+//The method should calculate the largest prime factor of a given number and return it.
+//
+//
+//
+//EXAMPLE INPUT/OUTPUT:
+//
+//getLargestPrime (21); should return 7 since 7 is the largest prime (3 * 7 = 21)
+//
+//getLargestPrime (217); should return 31 since 31 is the largest prime (7 * 31 = 217)
+//
+//getLargestPrime (0); should return -1 since 0 does not have any prime numbers
+//
+//getLargestPrime (45); should return 5 since 5 is the largest prime (3 * 3 * 5 = 45)
+//
+//getLargestPrime (-1); should return -1 since the parameter is negative
+//
+//
+//
+//HINT: Since the numbers 0 and 1 are not considered prime numbers, they cannot contain prime numbers.
+//
+//NOTE: The method getLargestPrime ​should be defined as public static like we have been doing so far in the course.
+//
+//NOTE: Do not add a main method to the solution code.
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
-}
