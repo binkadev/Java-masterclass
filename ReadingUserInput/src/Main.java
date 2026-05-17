4143396a9e177131c22ec518dfc327ce83269303
Trần Hoàng Hải ( Bin Dev ) <haihoang1312004@gmail.com>
2026-05-17 13:43:41 +0700

    Update Main.java

--- ReadingUserInput/src/Main.java
+++ ReadingUserInput/src/Main.java
@@ -12,6 +12,7 @@ public class Main {
             System.out.println(getInputFromScanner(currentYear));
         }
     }
+
     public static String getInputFromConsole(int currentYear) {
 
         String name = System.console().readLine("Hi, What's your Name? ");
@@ -35,11 +36,30 @@ public class Main {
 
         // String dateOfBirth = System.console().readLine("What year were you born? ");
         System.out.println("What year were you born? ");
-        String dateOfBirth = scanner.nextLine();
 
-        int age = currentYear - Integer.parseInt(dateOfBirth);
+        boolean validDob = false;
+        int age = 0;
+
+        do {
+            System.out.println("Enter a year of birth >= " +
+                    (currentYear - 125) + " and <= " + (currentYear));
+
+            age = checkData(currentYear, scanner.nextLine());
+            validDob = age < 0 ? false : true;
+        } while (!validDob);
 
         return "So you are " + age + " years old";
+    }
+
+    public static int checkData(int currentYear, String dateOfBirth) {
+        int dob = Integer.parseInt(dateOfBirth);
+        int minimumYear = currentYear - 125;
+
+        if ((dob < minimumYear) || (dob > currentYear)) {
+            return -1;
+        }
+
+        return (currentYear - dob);
     }
 }
 
